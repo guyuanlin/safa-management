e85d513498ccc8688a96f662af1b9cb2530ba74d
guyuan <lin.guyuan@gmail.com>
2013-04-23 00:32:30 +0800

    add a controller to control the products view

---
+++ src/main/java/safa/controller/ProductsViewController.java
@@ -0,0 +1,21 @@
+package safa.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class ProductsViewController implements Initializable {
+	
+	@FXML
+	private Button btnAddProduct;
+	
+	@FXML
+	private Button btnDeleteProduct;
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+}
